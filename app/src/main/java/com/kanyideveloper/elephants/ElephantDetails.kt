4278dc51a9e566cf7ebcc9d1d82ac3dd41306f72
JoelKanyi <joelkanyi98@gmail.com>
2020-07-24 16:36:54 +0300

    added the elephant details activity

---
+++ app/src/main/java/com/kanyideveloper/elephants/ElephantDetails.kt
@@ -0,0 +1,10 @@
+package com.kanyideveloper.elephants
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ElephantDetails : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
